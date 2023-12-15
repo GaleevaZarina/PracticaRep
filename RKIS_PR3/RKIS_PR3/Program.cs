ae2afcd2f5810dc4f6328be88cfbdff1db34ab4f
GaleevaZarina <galeevazarina888@mail.ru>
2023-12-16 04:00:06 +0700

    change sln

--- RKIS_PR3/RKIS_PR3/Program.cs
+++ RKIS_PR3/RKIS_PR3/Program.cs
@@ -9,64 +9,63 @@ namespace RKIS_PR3
     {
         public static void Main(string[] args)
         {
-            string fileName = @"D:\ТТИТ\РКИС\Практика\КОДЫ\input.txt";            
-            StreamReader reader = new StreamReader(fileName);                        
-            string[] lines = File.ReadAllLines(fileName);
-            reader.Close();
-            foreach (string s in lines)
+            string fileName = @"D:\ТТИТ\РКИС\Практика\КОДЫ\input.txt"; // Создаем переменную fileName, чтобы хранить в ней путь к файлу input          
+            StreamReader reader = new StreamReader(fileName); // Создаем поток для чтения файла input
+            string[] lines = File.ReadAllLines(fileName); // Читаем файл и записываем строки в массив lines
+                                                          // lines - массив строк
+            reader.Close(); // Закрываем поток
+            
+            foreach (string s in lines) // Выводим в консоль строки из файла input (из массива lines)
             {
                 Console.WriteLine(s);
-            }            
-            int[] luckyNumbers = new int[10];            
-            List<int[]> tickets = new List<int[]>();
-            for (int i = 0; i < lines.Length; i++)
+            }
+            
+            int[] luckyNumbers = new int[10]; // Создаем массив luckyNumbers для хранения в нем чисел выбранных орагнизаторами    
+            List<int[]> tickets = new List<int[]>(); // Создем список tickets для хранения в нем билетов (массивов с числами)
+            
+            for (int i = 0; i < lines.Length; i++) // Перебираем элементы (строки) в массиве lines
             {
-                int[] ticketsArray = new int [6];                
-                string strocaInLines = lines[i];
-                for (int k = 0; k < strocaInLines.Length; k++)
+                if (i == 0) // Если это первая строка (т.е. индекс = 0), то записываем ее в luckyNumbers
                 {
-                    if (i == 0)
-                    {
-                        luckyNumbers = strocaInLines.Split(' ').Select(int.Parse).ToArray();
-                    }                    
-                    else if (i > 1)
-                    {
-                        tickets.Add(strocaInLines.Split(' ').Select(int.Parse).ToArray());                        
-                        break;
-                    }
+                    luckyNumbers = lines[i].Split(' ').Select(int.Parse).ToArray();
+                }                    
+                else if (i > 1) // Если это третья (четвертая, пятая и т.д.) строка, то сохраняем ее в tickets
+                {
+                    tickets.Add(lines[i].Split(' ').Select(int.Parse).ToArray());
                 }
+
             }            
             
-            string fileName2 = @"D:\ТТИТ\РКИС\Практика\КОДЫ\output.txt";            
-            StreamWriter writer = new StreamWriter(fileName2);            
+            fileName = @"D:\ТТИТ\РКИС\Практика\КОДЫ\output.txt"; // Перезаписываем путь файла       
+            StreamWriter writer = new StreamWriter(fileName); // Создаем поток для записи информации в файл output       
             
-            foreach (var item in tickets)
+            foreach (var item in tickets) // Проходимся по билетам
             {
-                int luckyCount = 0;
-                for (int i = 0; i < item.Length; i++)
+                int luckyCount = 0; // Создаем счетчик luckyNumbers - считает количество счастливых чисел в билете
+                for (int i = 0; i < item.Length; i++) // Перебираем числа в билете
                 {
-                    for (int j = 0; j < luckyNumbers.Length; j++)
+                    for (int j = 0; j < luckyNumbers.Length; j++) // Перебираем числа выбранные организаторами и ищем их в билетах
                     {
-                        if (item[i] == luckyNumbers[j] && item[i] != ' ')
+                        if (item[i] == luckyNumbers[j]) // Если число в билете совпадает с выбранным организаторами, то увеличиваем счетчик на 1
                         {
                             luckyCount += 1;
                         }
                     }
                     
                 }
+                // После подсчета счастливых чисел в билете делаем проверку на выигрышный билет
                 if (luckyCount >= 3)
                 {
-                    writer.WriteLine("Lucky");
+                    writer.WriteLine("Lucky"); // Записываем результат в файл output
                 }
                 else
                 {
-                    writer.WriteLine("Unlucky");
+                    writer.WriteLine("Unlucky"); // Записываем результат в файл output
                 }
             }
             
-            writer.Close();
+            writer.Close(); // Закрываем поток
             
         }
     }
 }
-
